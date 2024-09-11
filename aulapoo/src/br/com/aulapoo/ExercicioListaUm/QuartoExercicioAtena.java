893ea6c456c0d5c87245f0ff13ef072b24998009
matheus lopes <matheuslopes19963@gmail.com>
2024-09-11 16:43:33 -0300

    Subindo exercicio lista um e dois Com Uso do Logger

--- aulapoo/src/br/com/aulapoo/ExercicioListaUm/QuartoExercicioAtena.java
+++ aulapoo/src/br/com/aulapoo/ExercicioListaUm/QuartoExercicioAtena.java
@@ -5,7 +5,8 @@ public class QuartoExercicioAtena {
 	public static void main(String[] args) {
 		
 		double tpC, tpF;
-		System.out.println(tpF = 30. * 1.8 + 32);
+		System.out.println("tpF = 30. * 1.8 + 32");
+		tpC=(tpF = 30. * 1.8 + 32);
 		System.out.println("Temperatura Fahrenheit: " + tpF);
 		
 
